6b5a773f8b9bbededb2131a989d98ee000f3a290
Sonam Pankaj <sonampankaj2@gmail.com>
2024-08-09 19:31:10 +0200

    first commit

---
+++ multimodal_RAG.py
@@ -0,0 +1,77 @@
+import requests
+import json
+
+from langchain_ollama.llms import OllamaLLM
+import embed_anything
+from pinecone import Pinecone, ServerlessSpec
+import numpy as np
+import os
+from pinecone import PineconeApiException
+import uuid
+from embed_anything import EmbedData
+from fastapi import FastAPI
+from pydantic import BaseModel
+from embed_anything.vectordb import PineconeAdapter
+import os
+from langchain_ollama import ChatOllama
+from langchain_core.messages import AIMessage
+from langchain.chains import LLMChain
+from fastapi import FastAPI, HTTPException
+
+from embed_anything.vectordb import PineconeAdapter
+
+
+ollama = OllamaLLM(base_url='http://localhost:11434', model='llama2')
+
+
+
+
+docs = embed_anything.embed_webpage(url="PUT-LINK", embeder="Jina")
+
+pc =  Pinecone(api_key="1f880c47-f636-4200-a76a-431ee51e2d48")
+
+
+
+
+
+
+
+
+
+
+# Initialize the PineconeEmbedder class
+api_key = os.environ.get("PINECONE_API_KEY")
+index_name = "anything"
+pinecone_adapter = PineconeAdapter(api_key)
+
+try:
+    pinecone_adapter.delete_index("anything")
+except:
+    pass
+
+# Initialize the PineconeEmbedder class
+
+pinecone_adapter.create_index(dimension=768, metric="cosine")
+
+
+
+app = FastAPI()
+
+class Query(BaseModel):
+    prompt: str
+
+@app.post("/ollama")
+async def run_ollama(query: Query):
+    embed_query = embed_anything.embed_query([query.prompt], embeder="Jina")
+    result = index_name.query(
+        vector=embed_query[0].embedding,
+        top_k=2,
+        include_metadata=True,
+    )
+    print(result)
+    metadata_texts = [item['metadata']['text'] for item in result['matches']]
+    
+    joined_text = ''.join(query.prompt) + ' '.join(metadata_texts[0])
+    response = ollama(joined_text)
+    return response
+
---
+++ readme.md
@@ -0,0 +1,29 @@
+
+
+<div align="center">
+
+  <p align="center">
+    <b>End-to-End, source to vector database multimodal-RAG </b>
+    <br />
+
+    
+  </p>
+</div>
+
+
+RAG-Anything is an end-to-end, from sources to vector database, multimodal-RAG system, powered by embed-anything 🦀
+
+
+
+
+
+
+
+
+
+
+
+run fastapi:
+
+``` fastapi dev ollama_demo.py```
+
